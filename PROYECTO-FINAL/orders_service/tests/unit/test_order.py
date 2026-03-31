27831dc54595594a2101709e1c8adeb9313597ac
IRVING DELGADO <irving.delgado@axity.com>
2026-03-30 19:59:52 -0600

    Update test to use InvalidQuantityError

--- PROYECTO-FINAL/orders_service/tests/unit/test_order.py
+++ PROYECTO-FINAL/orders_service/tests/unit/test_order.py
@@ -1,6 +1,6 @@
 import pytest
 from orders_service.domain.entities import Order
-
+from orders_service.domain.exceptions import InvalidQuantityError
 
 def test_create_order_success():
     order = Order.create("user1", "Laptop", 2)
@@ -12,5 +12,5 @@ def test_create_order_success():
 
 
 def test_create_order_invalid_quantity():
-    with pytest.raises(ValueError):
+    with pytest.raises(InvalidQuantityError):
         Order.create("user1", "Laptop", 0)
